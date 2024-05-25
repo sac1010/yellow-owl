1e8cd1e4af5e4ee83a1be248af2fb102c28035dd
sac1010 <sachingirish101@gmail.com>
2024-05-25 11:40:23 +0530

    env fix

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-// const BASE_URL =  "https://yellow-owl.onrender.com/" ;
-const BASE_URL =  "http://localhost:3000/" ;
+
+// const BASE_URL =  process.env.REACT_APP_API_BASE_URL ;
+const BASE_URL =  "https://yellow-owl.onrender.com/" ;
+
 
 
 const api = axios.create({
